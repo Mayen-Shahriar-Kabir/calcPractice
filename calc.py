0c64fa3d415138835bfc17e5a590b755292ce22d
Mayen-Shahriar-Kabir <mayenshahriarkabir111@gmail.com>
2024-06-09 18:21:56 +0600

    driverCode

---
+++ calc.py
@@ -0,0 +1,15 @@
+def Calculate():
+  s = input()
+  lst = s.split(" ")
+  lst[0], lst[2] = int(lst[0]), int(lst[2])
+
+  if lst[1] == "+":
+    print(add(lst[0], lst[2]))
+  elif lst[1] == "-":
+    print(subtract(lst[0], lst[2]))
+  elif lst[1] == "*":
+    print(multiply(lst[0], lst[2]))
+  elif lst[1] == "/":
+    print(divide(lst[0], lst[2]))
+
+Calculate()
